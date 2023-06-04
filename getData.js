74f05174c79b5a00ec2f50d8537054dd12f83f28
Dpbm <dpbm136@gmail.com>
2023-06-04 12:22:58 -0300

    setted default title

--- getData.js
+++ getData.js
@@ -1,11 +1,10 @@
 (() => {
-  if (window.hasRun) {
+  if (window.hasRun)
 		return;
-	}
 	window.hasRun = true;
 
-  browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const title = document.title;
+  browser.runtime.onMessage.addListener((_request, _sender, sendResponse) => {
+    const title = document.title || new Date().toString();
     
     const queryData = [...document.querySelectorAll("div.flex.flex-grow.gap-3")];
     const text = queryData.map((query) => query.innerText);
